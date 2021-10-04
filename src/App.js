c32c0e283152faa40b81650e5469935c1629d424
stevemolitor <steve.molitor@zapier.com>
2021-10-04 18:09:32 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello React Rendering</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Understanding React Rendering Process</h2>
     </div>
   );
 }
